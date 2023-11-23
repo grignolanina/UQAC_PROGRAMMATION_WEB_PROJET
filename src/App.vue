e824a61619b1cdbbbfd2ab69cf3b40f75a96d88d
grignolanina <grignola.nina@orange.fr>
2023-11-23 17:58:34 -0500

    debut component passewordgame + 1ere regles a respecter

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-  <p>Je rajoute du texte pour test un push</p>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+
+  <PasswordGame :correctPassword="correctPassword" :hint="hint" />
+
+  <!-- <div class="container">
+    <h1>Password Game</h1>
+    <p id="hint">Hint: Some Hint Text</p>
+    <input type="text" id="passwordInput" placeholder="Enter your guess">
+    <button onclick="checkPassword()">Submit</button>
+    <p id="result"></p>
+  </div> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import PasswordGame from './components/PasswordGame.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    PasswordGame
+  },
+  data() {
+    return {
+      correctPassword: "secret",
+      hint: "Some Hint Text"
+    }
   }
 }
+
+//CODE CHAT GPT 
+//const correctPassword = "secret";
+// const hint = "Some Hint Text";
+
+// function checkPassword() {
+//   const userInput = document.getElementById("passwordInput").value;
+//   const resultMessage = document.getElementById("result");
+
+//   if (userInput.toLowerCase() === correctPassword) {
+//     resultMessage.textContent = "Congratulations! You guessed the password!";
+//   } else {
+//     resultMessage.textContent = "Incorrect password. Try again.";
+//   }
+// }
+
+// document.getElementById("hint").textContent = "Hint: " + hint;
+
 </script>
 
 <style>
@@ -23,5 +59,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  text-align: center;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+button {
+  padding: 10px 20px;
+  margin-top: 10px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/PasswordGame.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="password-game">
+		<h1>Password Game</h1>
+		<p :text="'Hint: ' + hint"></p>
+		<input v-model="userInput" placeholder="Enter your guess">
+		<button @click="checkPassword">Submit</button>
+		<p>{{ result }}</p>
+	</div>
+</template>
+  
+<script>
+export default {
+	props: {
+		hint: String
+	},
+	data() {
+		return {
+			userInput: "",
+			result: ""
+		};
+	},
+	methods: {
+		checkPassword() {
+			if (this.isValidPassword(this.userInput)) {
+				this.result = "Congratulations! You entered a valid password!";
+			} else {
+				this.result = "Invalid password. Please check the rules and try again.";
+			}
+		},
+		isValidPassword(password) {
+			// Définissez vos règles de validation ici
+			// Exemple de règles : longueur minimale de 8 caractères, au moins une lettre majuscule et un chiffre
+			const minLength = 8;
+			const hasUppercase = /[A-Z]/.test(password);
+			const hasNumber = /\d/.test(password);
+
+			return password.length >= minLength && hasUppercase && hasNumber;
+		}
+	}
+};
+</script>
+  
+<style scoped></style>
+  
