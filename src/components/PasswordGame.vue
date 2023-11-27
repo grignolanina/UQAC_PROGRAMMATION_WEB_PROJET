7c750715493d0c5066d0ddb89bb6be80df6988e0
Lucie Augier <lucieaugier28@gmail.com>
2023-11-27 15:55:15 -0500

    règle n°9 fonctionnelle

--- src/components/PasswordGame.vue
+++ src/components/PasswordGame.vue
@@ -10,6 +10,7 @@
 <script>
 import ErrorMessage from "./ErrorMessage.vue";
 import SuccesMessage from "./SuccesMessage.vue";
+import { isIngredientPoutineWithUppercase, getDate, checkOdsNumbersRule} from "@/rules";
 
 export default {
 	components: {
@@ -26,6 +27,22 @@ export default {
 			isResolved: false,
 			resolvedMessages: [],
 			rules: [
+				{message : "Le deuxième et l'avant-dernier chiffre du mot de passe doivent être impairs.", test: password=> checkOdsNumbersRule(password)},
+				{ message:"Le mot de passe doit contenir la date du jour. (JJ/MM/AAAA))", test: password => new RegExp(getDate()).test(password)},
+				{ message: "L'ingrédient de la poutine s'écrit avec une majuscule ! On respecte la poutine !", 
+				test: password => {
+					const hasPoutineIngredients = /frites|sauce|fromageSquishSquish/i.test(password);
+					return isIngredientPoutineWithUppercase(hasPoutineIngredients, password);
+				}},
+				{ message: "Le mot de passe doit contenir au moins un ingrédient de la poutine : Frites, Sauce, FromageSquishSquish.", test: password => /frites|sauce|fromageSquishSquish/i.test(password) },
+				{message : "Le mot de passe doit contenir au moins une couleur de l'arc-en-ciel : rouge, orange, jaune, vert, bleu, violet.", test: password=> /rouge|orange|jaune|vert|bleu|violet/.test(password)},
+				{message : "La somme des chiffres du mot de passe doit être égale à 21.", 			test:password=>password
+									.split("") // Divise la chaîne en un tableau de caractères
+									.filter(char => /\d/.test(char)) // Filtre uniquement les chiffres
+									.reduce((acc, curr) => acc + parseInt(curr), 0) === 21},
+				{message : "Le mot de passe doit commencer par la lettre majuscule. Une phrase commence toujours par une majuscule !", test: password=> /^[A-Z]/.test(password)},
+				{message : "Le mot de passe doit contenir deux chiffres consécutifs.", test: password=> /\d{2}/.test(password)},
+				{message : "Le mot de passe doit contenir au moins un caractère spécial : !@#$%&?.", test: password=> /[!@#$%&?]+/.test(password)},
 				{ message: "Le mot de passe doit contenir au moins un chiffre.", test: password => /\d/.test(password) },
 				{ message: "Le mot de passe doit contenir au moins une lettre majuscule.", test: password => /[A-Z]/.test(password) },
 				{ message: "Le mot de passe doit avoir une longueur d'au moins 8 caractères.", test: password => password.length >= 8 },
@@ -69,11 +86,10 @@ export default {
 					this.resolvedMessages.push(`Résolu : ${message}`);
 				}
 			}
-
+			//console.log("Règles résolues :", this.resolvedMessages);
 			return failingRule ? `Échec : ${failingRule.message}` : "ok";
 		},
-
-	}
+	},
 };
 </script>
   
