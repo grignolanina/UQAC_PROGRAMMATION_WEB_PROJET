190271d1756de30abf6d19cad600207169b124e4
Lucie Augier <lucieaugier28@gmail.com>
2023-11-27 11:21:22 -0500

    10 règles

--- src/components/PasswordGame.vue
+++ src/components/PasswordGame.vue
@@ -12,7 +12,7 @@
 
 <script>
 import ErrorMessage from "./ErrorMessage.vue";
-import {getDate} from "../rules.js";
+import {/*getDate,*/ isSunsetTimeValid, getSunsetTime} from "../rules.js";
 export default {
 	components: {
 		ErrorMessage
@@ -24,8 +24,20 @@ export default {
 		return {
 			userInput: "",
 			result: "",
+			isSunsetTimeValid: isSunsetTimeValid
 		};
 	},
+
+	async created() {
+    try {
+      const sunsetTime = await getSunsetTime();
+      this.$data.isSunsetTimeValid = (password) =>
+        isSunsetTimeValid(password, sunsetTime);
+    } catch (error) {
+      console.error("Error fetching sunset time:", error);
+    }
+  },
+
 	methods: {
 		handleInput() {
 			if (this.isValidPassword(this.userInput)) {
@@ -34,6 +46,8 @@ export default {
 				this.result = this.rulesCheck(this.userInput);
 			}
 		},
+
+
 		isValidPassword(password) {
 			// const minLength = 8;
 			// const hasUppercase = /[A-Z]/.test(password);
@@ -46,20 +60,20 @@ export default {
 			// 							.filter(char => /\d/.test(char)) // Filtre uniquement les chiffres
 			// 							.reduce((acc, curr) => acc + parseInt(curr), 0) === 21; // Additionne les chiffres
 			//const hasRainbowColors = /rouge|orange|jaune|vert|bleu|violet/.test(password);
-			const hasPoutineIngredients = /frites|sauce|fromageSquishSquish/i.test(password);
-			const hasPoutineIngredientsWithUppercase = /frites|sauce|fromageSquishSquish/i.test(password) && /[A-Z]/.test(password);
-			const meetsRequirementsPoutine = hasPoutineIngredients && hasPoutineIngredientsWithUppercase;
-			const containsCurrentDate = new RegExp(getDate()).test(password);
-			const numericChars = password.match(/\d/g);
-			const secondAndBeforeLastDigitsAreOdd =
-				numericChars &&
-				numericChars.length >= 2 &&
-				parseInt(numericChars[1]) % 2 !== 0 &&
-				parseInt(numericChars[numericChars.length - 2]) % 2 !== 0;
-
+			// const hasPoutineIngredients = /frites|sauce|fromageSquishSquish/i.test(password);
+			// const hasPoutineIngredientsWithUppercase = /frites|sauce|fromageSquishSquish/i.test(password) && /[A-Z]/.test(password);
+			// const meetsRequirementsPoutine = hasPoutineIngredients && hasPoutineIngredientsWithUppercase;
+			// const containsCurrentDate = new RegExp(getDate()).test(password);
+			// const numericChars = password.match(/\d/g);
+			// const secondAndBeforeLastDigitsAreOdd =
+			// 	numericChars &&
+			// 	numericChars.length >= 2 &&
+			// 	parseInt(numericChars[1]) % 2 !== 0 &&
+			// 	parseInt(numericChars[numericChars.length - 2]) % 2 !== 0;
+			const hasSunsetTime = this.$data.isSunsetTimeValid(password);
 
 				
-			return /*password.length >= minLength && hasUppercase && hasNumber && hasSpecialCharacter && hasTwoConsecutiveNumbers && upperCaseIsFirst && isSumOfNumbers21 && hasRainbowColors && */hasPoutineIngredients && meetsRequirementsPoutine && containsCurrentDate && secondAndBeforeLastDigitsAreOdd;
+			return /*password.length >= minLength && hasUppercase && hasNumber && hasSpecialCharacter && hasTwoConsecutiveNumbers && upperCaseIsFirst && isSumOfNumbers21 && hasRainbowColors && hasPoutineIngredients && meetsRequirementsPoutine && containsCurrentDate && secondAndBeforeLastDigitsAreOdd && */hasSunsetTime;
 		},
 
 		rulesCheck(password) {
@@ -74,16 +88,18 @@ export default {
 			// 							.filter(char => /\d/.test(char)) // Filtre uniquement les chiffres
 			// 							.reduce((acc, curr) => acc + parseInt(curr), 0) === 21; // Additionne les chiffres
 			//const hasRainbowColors = /rouge|orange|jaune|vert|bleu|violet/.test(password);
-			const hasPoutineIngredients = /frites|sauce|fromageSquishSquish/i.test(password);
-			const hasPoutineIngredientsWithUppercase = /frites|sauce|fromageSquishSquish/i.test(password) && /[A-Z]/.test(password);
-			const meetsRequirementsPoutine = hasPoutineIngredients && hasPoutineIngredientsWithUppercase;
-			const containsCurrentDate = new RegExp(getDate()).test(password);
-			const numericChars = password.match(/\d/g);
-			const secondAndBeforeLastDigitsAreOdd =
-				numericChars &&
-				numericChars.length >= 2 &&
-				parseInt(numericChars[1]) % 2 !== 0 &&
-				parseInt(numericChars[numericChars.length - 2]) % 2 !== 0;
+			// const hasPoutineIngredients = /frites|sauce|fromageSquishSquish/i.test(password);
+			// const hasPoutineIngredientsWithUppercase = /frites|sauce|fromageSquishSquish/i.test(password) && /[A-Z]/.test(password);
+			// const meetsRequirementsPoutine = hasPoutineIngredients && hasPoutineIngredientsWithUppercase;
+			// const containsCurrentDate = new RegExp(getDate()).test(password);
+			// const numericChars = password.match(/\d/g);
+			// const secondAndBeforeLastDigitsAreOdd =
+			// 	numericChars &&
+			// 	numericChars.length >= 2 &&
+			// 	parseInt(numericChars[1]) % 2 !== 0 &&
+			// 	parseInt(numericChars[numericChars.length - 2]) % 2 !== 0;
+			const hasSunsetTime = this.$data.isSunsetTimeValid(password);
+			
 			// if (password.length < minLength) {
 			// 	return "Le mot de passe doit avoir une longueur d'au moins 8 caractères.";
 			// }
@@ -110,22 +126,27 @@ export default {
 			// if (!hasRainbowColors) {
 			// 	return "Le mot de passe doit contenir au moins une couleur de l'arc-en-ciel : rouge, orange, jaune, vert, bleu, violet.";
 			// }
-			if (!hasPoutineIngredients) {
-				return "Le mot de passe doit contenir au moins un ingrédient de la poutine : Frites, Sauce, FromageSquishSquish.";
-			}
-			if (!meetsRequirementsPoutine) {
-				return "L'ingrédient de la poutine s'écrit avec une majuscule ! On respecte la poutine !";
-			}
-			if (!containsCurrentDate) {
-				return "Le mot de passe doit contenir la date du jour. (JJ/MM/AAAA))";
-			}
-			if (!secondAndBeforeLastDigitsAreOdd) {
-				return "Le deuxième et l'avant-dernier chiffre du mot de passe doivent être impairs.";
+			// if (!hasPoutineIngredients) {
+			// 	return "Le mot de passe doit contenir au moins un ingrédient de la poutine : Frites, Sauce, FromageSquishSquish.";
+			// }
+			// if (!meetsRequirementsPoutine) {
+			// 	return "L'ingrédient de la poutine s'écrit avec une majuscule ! On respecte la poutine !";
+			// }
+			// if (!containsCurrentDate) {
+			// 	return "Le mot de passe doit contenir la date du jour. (JJ/MM/AAAA))";
+			// }
+			// if (!secondAndBeforeLastDigitsAreOdd) {
+			// 	return "Le deuxième et l'avant-dernier chiffre du mot de passe doivent être impairs.";
+			// }
+			if (!hasSunsetTime) {
+				return "Le mot de passe doit contenir l'heure du coucher du soleil à cet endroit : lat=45.504814179128, lng=-73.58723572207455, en UTC. (HH:MM) (En UTC hihi)";
 			}
 
 
 			return "error";
-		}
+		},
+
+
 	}
 };
 </script>
